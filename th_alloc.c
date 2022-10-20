4d4718d949e64301c247abcc79584fd8dc76296b
tedurand <tedurand@live.unc.edu>
2022-10-19 22:22:24 -0400

    asdfasdfs

--- th_alloc.c
+++ th_alloc.c
@@ -314,25 +314,26 @@ void free(void *ptr) {
     struct superblock_bookkeeping * sb_cur;
     struct superblock_bookkeeping * sb_prev;
     struct superblock_bookkeeping * sb_next;
-    while (levels[power].whole_superblocks > RESERVE_SUPERBLOCK_THRESHOLD) {
-        sb_prev = NULL;
-        sb_cur = levels[power].next;
-        while(sb_cur){
+    sb_prev = NULL;
+    sb_cur = levels[power].next;
+    while (levels[power].whole_superblocks > RESERVE_SUPERBLOCK_THRESHOLD) {      
+        // while(sb_cur){
             sb_next = sb_cur->next;
             if(sb_cur->free_count == max_free_objects){
                 if(!sb_prev){
                     levels[power].next = sb_next;
                 } else {
-                    sb_prev->next = sb_next;
+                    sb_prev = sb_cur;
+                    sb_cur = sb_next;
                 }
-                munmap(sb_cur, SUPER_BLOCK_SIZE);
+                munmap(sb_prev, SUPER_BLOCK_SIZE);
                 levels[power].whole_superblocks--;
                 levels[power].free_objects-= max_free_objects;
             } else {
                 sb_prev = sb_cur;
                 sb_cur = sb_next;
             }
-        }        
+     //   }        
     }
 
 }
